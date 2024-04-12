21f4d288cddc51e81b2d7f1291e47097df12df6c
iufb <misakilover228@gmail.com>
2024-04-12 08:45:14 +0500

    feat: lang switcher

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,24 +3,5 @@ import { Section } from "../Section/Section";
 
 export const Hero = () => {
   const { t } = useTranslation();
-  return (
-    <Section>
-      {t("hello")}
-      <LanguageSwitcher />
-    </Section>
-  );
+  return <Section>{t("hello")}</Section>;
 };
-function LanguageSwitcher() {
-  const { i18n } = useTranslation();
-
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-
-  return (
-    <div>
-      <button onClick={() => changeLanguage("en")}>English</button>
-      <button onClick={() => changeLanguage("ru")}>Ru</button>
-    </div>
-  );
-}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
+import { useTranslation } from "react-i18next";
 import { Button } from "../Button/Button";
 import styles from "./Navbar.module.css";
 
 import logo from "/ais-logo-rs.png";
+import { useEffect, useState } from "react";
 export const Navbar = () => {
   return (
     <nav className={styles.wrapper}>
@@ -9,10 +11,36 @@ export const Navbar = () => {
         <img src={logo} alt="Ais logo" className={styles.logo} />
       </div>
       <div className={styles.right}>
+        <LanguageSwitcher />
         <Button isLink={true} href="#form" variant="primary">
           Оставить заявку
         </Button>
       </div>
     </nav>
+  );
+};
+
+const LanguageSwitcher = () => {
+  const { i18n } = useTranslation();
+  const [lang, setLang] = useState(false);
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+  const selectedLang = lang ? "ru" : "en";
+  useEffect(() => {
+    changeLanguage(lang ? "ru" : "en");
+  }, [lang]);
+
+  return (
+    <label className={styles.switch}>
+      <input
+        type="checkbox"
+        checked={lang}
+        onChange={(e) => {
+          setLang(!lang);
+        }}
+      />
+      <span data-lang={selectedLang.toUpperCase()} className={styles.slider} />
+    </label>
   );
 };
