251bef027c639b140f33d2ef4011afcb49344b6d
iufb <bashirov3ld2@gmail.com>
2025-06-27 09:39:22 +0500

    hide contact us

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
 import "./App.css";
 import {
-  About,
-  Admission,
-  ContactUs,
-  Faces,
-  FloatButtons,
-  Footer,
-  Hero,
-  Labs,
-  Navbar,
-  Partners,
-  Programs,
-  Unique,
+    About,
+    Admission,
+    Faces,
+    FloatButtons,
+    Footer,
+    Hero,
+    Labs,
+    Navbar,
+    Partners,
+    Programs,
+    Unique
 } from "./components";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <main className="rootContent">
-        <About />
-        <Faces />
-        <Unique />
-        <Labs />
-        <Programs />
-        <Admission />
-        <ContactUs />
-        <Partners />
-      </main>
-      <Footer />
-      <FloatButtons />
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <main className="rootContent">
+                <About />
+                <Faces />
+                <Unique />
+                <Labs />
+                <Programs />
+                <Admission />
+                {/* <ContactUs /> */}
+                <Partners />
+            </main>
+            <Footer />
+            <FloatButtons />
+        </>
+    );
 }
 
 export default App;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,73 +7,74 @@ import { Text } from "../Text/Text";
 import styles from "./Hero.module.css";
 const heroBgs = ["/herobg-light.webp", "/ch/2.webp"];
 const slideUp = {
-  hidden: {
-    opacity: 0,
-    y: 200,
-  },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 1.4,
-      ease: "easeInOut",
+    hidden: {
+        opacity: 0,
+        y: 200,
     },
-  },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1.4,
+            ease: "easeInOut",
+        },
+    },
 };
 export const Hero = () => {
-  const { t } = useTranslation();
-  const [current, setCurrent] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrent((prev) => {
-        if (prev == heroBgs.length - 1) {
-          return 0;
-        } else {
-          return prev + 1;
-        }
-      });
-    }, 7000);
-    return () => clearInterval(interval);
-  }, []);
-  return (
-    <Section fullWidth>
-      <section
-        style={{
-          backgroundImage: `url(${heroBgs[current]})`,
-        }}
-        className={styles.bg}
-      >
-        <section className={styles.blur} />
-        <motion.section
-          initial="hidden"
-          animate="visible"
-          variants={slideUp}
-          className={styles.content}
-        >
-          <Text tag={"h1"} color="base" variant={"title"}>
-            {t("hero.title")}
-          </Text>
-          <Text tag={"h2"} color="base" variant={"title"}>
-            {t("hero.subtitle")}
-          </Text>
-          <Text tag={"h4"} color="base" variant={"title"}>
-            {t("hero.subsubtitle")}
-          </Text>
-          <Text tag={"h5"} color="base" variant={"title"}>
-            {t("hero.text")}
-          </Text>
+    const { t } = useTranslation();
+    const [current, setCurrent] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent((prev) => {
+                if (prev == heroBgs.length - 1) {
+                    return 0;
+                } else {
+                    return prev + 1;
+                }
+            });
+        }, 7000);
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <Section fullWidth>
+            <section
+                style={{
+                    backgroundImage: `url(${heroBgs[current]})`,
+                }}
+                className={styles.bg}
+            >
+                <section className={styles.blur} />
+                <motion.section
+                    initial="hidden"
+                    animate="visible"
+                    variants={slideUp}
+                    className={styles.content}
+                >
+                    <Text tag={"h1"} color="base" variant={"title"}>
+                        {t("hero.title")}
+                    </Text>
+                    <Text tag={"h2"} color="base" variant={"title"}>
+                        {t("hero.subtitle")}
+                    </Text>
+                    <Text tag={"h4"} color="base" variant={"title"}>
+                        {t("hero.subsubtitle")}
+                    </Text>
+                    <Text tag={"h5"} color="base" variant={"title"}>
+                        {t("hero.text")}
+                    </Text>
 
-          <Button
-            href="#form"
-            isLink
-            className={styles.button}
-            variant={"outline"}
-            color="base"
-          >
-            {t("buttons.contact")}
-          </Button>
-        </motion.section>
-      </section>
-    </Section>
-  );
+                    <Button
+                        href="https://wa.me/77089383180"
+                        isLink
+                        target="_blank"
+                        className={styles.button}
+                        variant={"outline"}
+                        color="base"
+                    >
+                        {t("buttons.contact")}
+                    </Button>
+                </motion.section>
+            </section>
+        </Section>
+    );
 };
