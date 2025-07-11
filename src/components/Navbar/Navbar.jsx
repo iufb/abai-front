1b3976a6b600a0d6fcfb5a2823e6a1ed88a29126
iufb <bashirov3ld2@gmail.com>
2025-07-11 16:12:40 +0500

    wa to navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,71 +6,73 @@ import { useSelectedLang } from "../../utils";
 import { BurgerButton } from "../BurgerButton/BurgerButton";
 import logo from "/ais-logo.webp";
 export const Navbar = () => {
-  const { t } = useTranslation();
-  return (
-    <header className={styles.wrapper}>
-      <nav className={styles.nav}>
-        <a
-          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-          className={styles.logoWrapper}
-        >
-          <img src={logo} alt="Ais logo" className={styles.logo} />
-        </a>
-        <div className={styles.right}>
-          <section className={styles.buttons}>
-            <a className={styles.link} href="#about">
-              {t("about.title")}
-            </a>
-            {/* <Button
+    const { t } = useTranslation();
+    return (
+        <header className={styles.wrapper}>
+            <nav className={styles.nav}>
+                <a
+                    onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                    className={styles.logoWrapper}
+                >
+                    <img src={logo} alt="Ais logo" className={styles.logo} />
+                </a>
+                <div className={styles.right}>
+                    <section className={styles.buttons}>
+                        <a className={styles.link} href="#about">
+                            {t("about.title")}
+                        </a>
+                        {/* <Button
               isLink={true}
               href="#admission"
               variant="outline"
               className={styles.applicationBtn}
             > */}
-            <a className={styles.link} href="#admission">
-              {t("buttons.admission")}
-            </a>
+                        <a className={styles.link} href="#admission">
+                            {t("buttons.admission")}
+                        </a>
 
-            <Button
-              isLink={true}
-              color="base"
-              href="#form"
-              variant="primary"
-              className={styles.applicationBtn}
-            >
-              {t("buttons.contact")}
-            </Button>
-          </section>
-        </div>
-        <div className={styles.langPhone}>
-          <a className={styles.phone} href={"tel:+7 708 938 3180"}>
-            <img className={styles.phoneIcon} src="/phone.svg" alt="call" />
-            <span>+7 708 938 3180</span>
-          </a>
-          <LanguageSwitcher />
-        </div>
-        <BurgerButton className={styles.burger} />
-      </nav>
-    </header>
-  );
+                        <Button
+                            target='_blank'
+
+                            isLink={true}
+                            color="base"
+                            href="https://wa.me/77089383180"
+                            variant="primary"
+                            className={styles.applicationBtn}
+                        >
+                            {t("buttons.contact")}
+                        </Button>
+                    </section>
+                </div>
+                <div className={styles.langPhone}>
+                    <a className={styles.phone} href={"tel:+7 708 938 3180"}>
+                        <img className={styles.phoneIcon} src="/phone.svg" alt="call" />
+                        <span>+7 708 938 3180</span>
+                    </a>
+                    <LanguageSwitcher />
+                </div>
+                <BurgerButton className={styles.burger} />
+            </nav>
+        </header>
+    );
 };
 
 const LanguageSwitcher = () => {
-  const { lang, setLang, selectedLang, notSelected } = useSelectedLang();
-  return (
-    <label className={styles.switch}>
-      <input
-        type="checkbox"
-        checked={lang}
-        onChange={() => {
-          setLang(!lang);
-        }}
-      />
-      <span
-        data-notactive={notSelected}
-        data-lang={selectedLang}
-        className={styles.slider}
-      />
-    </label>
-  );
+    const { lang, setLang, selectedLang, notSelected } = useSelectedLang();
+    return (
+        <label className={styles.switch}>
+            <input
+                type="checkbox"
+                checked={lang}
+                onChange={() => {
+                    setLang(!lang);
+                }}
+            />
+            <span
+                data-notactive={notSelected}
+                data-lang={selectedLang}
+                className={styles.slider}
+            />
+        </label>
+    );
 };
